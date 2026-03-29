4a64a10b79fb151e0621aa903ea51fea5beeeb96
Injoon Oh <injoon5@naver.com>
2026-03-29 13:07:25 +0900

    Update cron jobs to use new school codes

--- packages/backend/convex/crons.ts
+++ packages/backend/convex/crons.ts
@@ -7,28 +7,28 @@ crons.hourly(
   "fetch timetable - this week",
   { minuteUTC: 0 },
   internal.timetable.fetchAndSave,
-  { grade: 3, classno: 4, week: 0, schoolcode: "7081492" }
+  { grade: 1, classno: 3, week: 0, schoolcode: "7010208" }
 );
 
 crons.hourly(
   "fetch timetable - next week",
   { minuteUTC: 1 },
   internal.timetable.fetchAndSave,
-  { grade: 3, classno: 4, week: 1, schoolcode: "7081492" }
+  { grade: 1, classno: 3, week: 1, schoolcode: "7010208" }
 );
 
 crons.hourly(
   "fetch meals - this week",
   { minuteUTC: 0 },
   internal.meals.fetchCurrentWeek,
-  { schoolcode: "7081492" }
+  { schoolcode: "7010208" }
 );
 
 crons.hourly(
   "fetch meals - next week",
   { minuteUTC: 1 },
   internal.meals.fetchNextWeek,
-  { schoolcode: "7081492" }
+  { schoolcode: "7010208" }
 );
 
 export default crons;
